b4389ca635a26a0dc2b242fd07648e6e77630ba4
Reza <rezapython@gmail.com>
2016-08-30 17:32:49 +0430

    Creating a log-in view

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # post views
+    url(r'^login/$', views.user_login, name='login'),
+
+    # login / logout urls
+    
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(requset):
+    if requset.method == 'POST':
+        form = LoginForm(requset.POST)
+
+        '''
+        Note the difference between authenticate and login:
+        authenticate() checks user credentials and returns a user object
+        if they are right; login() sets the user in the current session.
+        '''
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(requset, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    form = LoginForm()
+    return render(requset, 'account/login.html', {'form': form})
