d2087fadeadaf5a70fd1cc476f3f6e0b0da362fe
Reza <rezapython@gmail.com>
2016-09-01 01:32:33 +0430

    Reset password views

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -1,6 +1,7 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
