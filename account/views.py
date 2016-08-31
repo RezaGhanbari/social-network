6f4edf44261224283f4ed79678393274abe73eaf
Reza <rezapython@gmail.com>
2016-08-31 13:29:09 +0430

    Log-in and Lo-out and Dashboard views and templates.

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(requset):
@@ -28,3 +29,10 @@ def user_login(requset):
                 return HttpResponse('Invalid login')
     form = LoginForm()
     return render(requset, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
