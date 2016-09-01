8c346f2ad94a875a4fdf4fcb79784aa42e118e33
Reza <rezapython@gmail.com>
2016-09-01 21:27:04 +0430

    User registration and user profiles

--- account/urls.py
+++ account/urls.py
@@ -36,6 +36,9 @@ urlpatterns = [
 
     url(r'^password-reset/complete/$',
         'django.contrib.auth.views.password_reset_confirm',
-        name='password_reset_complete')
+        name='password_reset_complete'),
+
+    # registration urls
+    url(r'^register/$', views.register, name='register'),
 
 ]
